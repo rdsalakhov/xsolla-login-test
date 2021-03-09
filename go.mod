26df795afb0a8119a58e6c889638488bde06bfe7
rdsalakhov <salakhov.roman12@gmail.com>
2021-03-09 11:39:32 +0500

    fixed error: token used before issued

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.15
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	github.com/gorilla/handlers v1.5.1 // indirect
 	github.com/gorilla/mux v1.8.0
 	golang.org/x/oauth2 v0.0.0-20210220000619-9bb904979d93
 )
--- services/authService.go
+++ services/authService.go
@@ -13,25 +13,25 @@ import (
 type AuthService struct {
 	oAuthConfig *oauth2.Config
 	randomState string
-	jwtAccess string
+	jwtAccess   string
 	userInfoURL func(userId string) string
 }
 
 func NewAuthService() *AuthService {
 	return &AuthService{
 		oAuthConfig: &oauth2.Config{
-		RedirectURL:  "http://localhost:5000/callback",
-		ClientID:     "310",
-		ClientSecret: "qOorjUDbg5OLNi7nNCOQrrrbOHA6KgLV",
-		Endpoint: oauth2.Endpoint{
-			AuthURL:   "https://login.xsolla.com/api/oauth2/login",
-			TokenURL:  "https://login.xsolla.com/api/oauth2/token",
-			AuthStyle: 0,
+			RedirectURL:  "http://localhost:5000/callback",
+			ClientID:     "310",
+			ClientSecret: "qOorjUDbg5OLNi7nNCOQrrrbOHA6KgLV",
+			Endpoint: oauth2.Endpoint{
+				AuthURL:   "https://login.xsolla.com/api/oauth2/login",
+				TokenURL:  "https://login.xsolla.com/api/oauth2/token",
+				AuthStyle: 0,
 			},
 		},
 		randomState: "myrandom",
-		jwtAccess: "QtGwsOOJHxepeqrJ7IJaMSZZFrwIyYp4Xz1JK2koDgDJIqxDJ4bm94iHNkCcyUQp",
-		userInfoURL: func(userId string) string{
+		jwtAccess:   "QtGwsOOJHxepeqrJ7IJaMSZZFrwIyYp4Xz1JK2koDgDJIqxDJ4bm94iHNkCcyUQp",
+		userInfoURL: func(userId string) string {
 			return fmt.Sprintf("https://login.xsolla.com/api/users/%s/public", userId)
 		},
 	}
@@ -42,7 +42,7 @@ func (s *AuthService) IsValidState(state string) bool {
 }
 
 func (s *AuthService) ExchangeCode(code string) (*oauth2.Token, error) {
-	var token , err = s.oAuthConfig.Exchange(context.Background(), code)
+	var token, err = s.oAuthConfig.Exchange(context.Background(), code)
 	if err != nil {
 		err = fmt.Errorf("failed to exchange code for token: %s", err)
 		return nil, err
@@ -55,6 +55,11 @@ func (s *AuthService) GetInfoFromJWT(token string) (map[string]interface{}, erro
 	if _, err := jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
 		return []byte(s.jwtAccess), nil
 	}); err != nil {
+		// sometimes tokens come signed with sign date parameter greater than current system time
+		// maybe because system time and auth server time are not synced
+		if err.Error() == "Token used before issued" {
+			return claims, nil
+		}
 		err = fmt.Errorf("failed to parse jwt: %s", err)
 		return nil, err
 	}
@@ -76,7 +81,7 @@ func (s *AuthService) GetInfoFromHttp(userId string, token string) (string, erro
 	}
 	defer res.Body.Close()
 	body, err := ioutil.ReadAll(res.Body)
-	if err != nil{
+	if err != nil {
 		err = fmt.Errorf("could not parse response : %s", err.Error())
 		return "", err
 	}
